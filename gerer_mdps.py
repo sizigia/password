101557ac3da6e2627263a3cc888d577af188ba5e
Faustina Maria <faustina.giaquinta@gmail.com>
2023-04-14 09:31:58 +0200

    flux créé pour le programme de gestion de mot de passe

---
+++ gerer_mdps.py
@@ -0,0 +1,42 @@
+def afficher_menu():
+    """
+    """
+
+    print("""GESTION DES MOTS DE PASSE.
+    Menu :
+    1. Ajouter un nouveau mot de passe
+    2. Afficher tous les mots de passe
+    3. Quitter
+    """)
+# end def
+
+def ajouter_mdp():
+    """
+    """
+    print()
+    return None
+# end def
+
+def afficher_mdp():
+    """
+    """
+    print()
+    return None
+# end def
+
+afficher_menu()
+choix = input("Veuillez choisir une option : ")
+
+options = {
+    "1": ajouter_mdp,
+    "2": afficher_mdp,
+    "3": exit
+}
+
+if choix in options:
+    options[choix]()
+else:
+    print("Option invalide. Veuillez réessayer.")
+    while not choix in options:
+        afficher_menu()
+        choix = input("Veuillez choisir une option : ")
